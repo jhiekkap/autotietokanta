a447d614d583157e3a66bba749ab6c9cc364a2b1
jhiekkap <jari.hiekkapelto@gmail.com>
2019-05-10 15:11:02 +0300

    2

--- tietovarasto/luontilauseet.sql
+++ tietovarasto/luontilauseet.sql
@@ -1,14 +1,40 @@
-DROP DATABASE IF EXISTS henkilokanta;
-CREATE DATABASE henkilokanta;
-USE henkilokanta;
-CREATE USER IF NOT EXISTS 'saku'@'localhost' IDENTIFIED BY 'salainen';
-CREATE TABLE henkilo(
-henkiloId INTEGER NOT NULL PRIMARY KEY,
-etunimi VARCHAR(20) NOT NULL,
-sukunimi VARCHAR(30) NOT NULL,
-osasto VARCHAR(15),
-palkka DECIMAL(6,2)
+DROP DATABASE IF EXISTS autotietokanta;
+CREATE DATABASE autotietokanta;
+USE autotietokanta;
+CREATE USER IF NOT EXISTS 'lars'@'localhost' IDENTIFIED BY 'kCp1cjmJ';
+CREATE TABLE autot (
+valmistusnro integer not null,
+nimi varchar(12) not null,
+rekisteriNro varchar(6) not null,
+arvostelu varchar(8) not null,
+varastoLkm integer not null
 );
-GRANT ALL PRIVILEGES ON henkilokanta.* TO 'saku'@'localhost';
-INSERT INTO henkilo VALUES(1,'Leila','Hökki','atk',3000);
-INSERT INTO henkilo VALUES(2,'Pirkko','Puro','hallinto',7000);
+GRANT ALL PRIVILEGES ON autotietokanta.* TO 'lars'@'localhost';
+INSERT INTO autot VALUES(1,'Mersu','ABC-123','*****',20);
+INSERT INTO autot VALUES(2,'Mosse','QWE-321','**',7000);
+
+
+
+
+USE G3ASSd95JP;
+CREATE USER IF NOT EXISTS 'G3ASSd95JP' IDENTIFIED BY 'VCVohcxWkg';
+CREATE TABLE autot (
+valmistusnro integer not null,
+nimi varchar(12) not null,
+rekisteriNro varchar(6) not null,
+arvostelu varchar(8) not null,
+varastoLkm integer not null
+);
+GRANT ALL PRIVILEGES ON henkilokanta.* TO 'G3ASSd95JP';
+INSERT INTO autot VALUES(1,'Mersu','ABC-123','*****',20);
+INSERT INTO autot VALUES(2,'Mosse','QWE-321','**',7000);
+
+Username: G3ASSd95JP
+
+Password: VCVohcxWkg
+
+Database Name: G3ASSd95JP
+
+Server: remotemysql.com
+
+Port: 3306
